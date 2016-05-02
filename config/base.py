ce28df4ab26267399d10952f8e509008017ea73f
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-05-02 07:36:37 -0400

    Ensure that we use the correct service name when attempting to obtain the IP to the publishing service.

--- config/base.py
+++ config/base.py
@@ -1,7 +1,7 @@
 # General
 stopword_file_path = "stopwords_norwegian.txt"
 comm_host = "http://127.0.0.1:9001/" # DHT host
-content_module_name = "publish" # Name of the content module in the DHT.
+content_module_name = "publishing" # Name of the content module in the DHT.
 
 # Postgres database
 db_host = "despina.128.no"
